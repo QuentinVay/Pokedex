ea3077976185ec69e4722129ef3ba988d920ef0c
Quentin Vayssieres <quentin.vayssieres@gmail.com>
2023-10-11 18:42:54 +0200

    Message pika pika

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,26 @@
+import { useEffect } from "react";
 
 function NavBar({ pokemonList, pokemonIndex, setPokemonIndex }) {
 
+    const handleClick = (index) => {
+        if (pokemonList[index].name === "Pikachu") {
+            alert("pika pikachu !!!");
+        }
+        setPokemonIndex(index);
+    }
 
     return (
         <div>
             {pokemonList.map((pokemon, index) => (
                 <button
                     key={index}
-                    onClick={() => setPokemonIndex(index)}
+                    onClick={() => handleClick(index)}
                 >
                     {pokemon.name}
                 </button>
             ))}
+
+
         </div>
     );
 }
