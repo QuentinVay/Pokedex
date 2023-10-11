e6acac7fb58c69199887333af5d9be03b7e7b283
Quentin Vayssieres <quentin.vayssieres@gmail.com>
2023-10-11 15:47:15 +0200

    The NavBar component displays one button per pokemon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,17 @@
 
 function NavBar({ pokemonList, pokemonIndex, setPokemonIndex }) {
 
-    const handleClick = () => {
-        if (pokemonIndex < pokemonList.length - 1) {
-            setPokemonIndex(pokemonIndex + 1);
-        }
-    }
 
-    const handleClickA = () => {
-        if (pokemonIndex > 0) {
-            setPokemonIndex(pokemonIndex - 1);
-        }
-
-    }
     return (
         <div>
-            < button onClick={handleClickA} >précédent</button>
-            <button onClick={handleClick}>suivant</button>
+            {pokemonList.map((pokemon, index) => (
+                <button
+                    key={index}
+                    onClick={() => setPokemonIndex(index)}
+                >
+                    {pokemon.name}
+                </button>
+            ))}
         </div>
     );
 }
